6e2ec94a170cbe9150ad5885259eb53ad682b780
WoundedMighty212 <michael.ludick13@outlook.com>
2024-08-26 05:50:50 +0200

    second commit

---
+++ RegexPracticeJava-master/src/java_simpleregistry/Java_SimpleRegistry.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_simpleregistry;
+
+/**
+ * Please use the Registration_Form that is where the code is and 
+ * the entry point to this application.
+ * @author Albert Michael Ludick
+ */
+public class Java_SimpleRegistry {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
